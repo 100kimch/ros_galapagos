a08eb3d3388e09a55760dc3ae3d53b401520d13f
Kim Jihyeong <100kimch@naver.com>
2020-01-14 18:19:07 +0900

    added installation script

---
+++ setup/install_galapagos_lite.sh
@@ -0,0 +1,10 @@
+sudo rm -rdf ~/catkin_ws/src/galapagos_lite/
+cp -r ../packages/galapagos_lite ~/catkin_ws/src/galapagos_lite/
+cd ~/catkin_ws/src/galapagos_lite/
+sudo chmod +x .
+sudo chmod launch/*.launch
+cd ~/catkin_ws/
+catkin_make
+source devel/setup.bash
+cd ~
+source ~/.bashrc
